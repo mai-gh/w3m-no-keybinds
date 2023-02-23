10d6c919f1dbe54b89adb06986907d7d1290096a
Mai <jea989@gmail.com>
2023-02-23 14:57:00 -0500

    clean debug script

--- add_debug.py
+++ add_debug.py
@@ -3,23 +3,17 @@
 from sys import argv
 
 with open(argv[1], 'r') as f:
-    inp = f.read().splitlines()
+  inp = f.read().splitlines()
 
 outp = []
 i = 0
+
 for line in inp:
   if len(line) > 0:
-#    print(len(line))
     if line[0] == '{':
-#    print("aaa", line, "bbb")
-     # fprintf( stderr, "my %s has %d chars\n", "string format", 30);
      line += 'fprintf(stderr, "{}\\n");'.format(inp[i-1].split('(')[0])
-      
   outp.append(line)
-
   i += 1
 
 with open(argv[2], "w") as f:
   print(*outp, sep="\n", file=f)
-
-
